40ee1b597b90d09da815e8938c471565fcc2d032
kat zhang <kzhang193@gmail.com>
2018-12-20 20:40:38 -0600

    double check heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,13 +2,10 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/", function (req, res) {
-        console.log("jgjgjg");
-        // res.send("Arnab ie the best")
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     app.get("/survey", function (req, res) {
-        // res.send("Kat is the best!")
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 }
--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-// var express = require("express");
 
-// var app = express();
 
 var PORT = process.env.PORT || 3055;
 
-// app.use(express.static("public"));
-
-// app.use(express.urlencoded({extended:true}));
-// app.use(express.json());
-
-
-// var exphbs = require("express-handlebars");
-// app.engine("handlebars", exphbs({defaultLayout: "main"}));
-// app.set("view engine", "handlebars");
 var express = require('express');
 
 var app = express();
@@ -35,9 +24,7 @@ connection.connect(function (error) {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
 app.use(express.static("./app/public"));
-
 
 
 // require("./app/routing/apiRoutes")(app);
@@ -53,18 +40,6 @@ app.get("/", function(req,res) {
         res.render("home");
     });
 });
-
-// app.get("/survey", function(req,res) {
-//     connection.query("SELECT * FROM questions;", function(error, data) {
-//         if (error) {
-//             return res.status(500).end();
-//         }
-//         res.render("survey", { questions: data});
-//     });
-// });
-
-
-
 
 
 
