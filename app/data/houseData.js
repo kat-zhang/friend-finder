e74864a81f82aaec15bce87834e930b9ced5f1ca
kat zhang <kzhang193@gmail.com>
2018-12-20 22:42:52 -0600

    i dont understand the api

--- app/data/houseData.js
+++ app/data/houseData.js
@@ -1,27 +1,23 @@
 var houses = [
     {
         house: "Slytherin",
-        val: 1,
+        values: [3, 3, 5, 1, 1, 4, 5, 4, 5, 5],
         image: "https://raw.githubusercontent.com/kat-zhang/house_match/master/app/public/assets/images/slytherin.jpg"
     },
     {
         house: "Ravenclaw",
-        val: 2,
+        values: [5, 4, 5, 2, 2, 5, 3, 5, 4, 4],
         image: "https://raw.githubusercontent.com/kat-zhang/house_match/master/app/public/assets/images/ravenclaw.jpg"
     },
+   
     {
-        house: "Muggle",
-        val: 3,
-        image: "https://raw.githubusercontent.com/kat-zhang/house_match/master/app/public/assets/images/muggle.jpg"
-    },
-    {
-        house: "Gryffindor"
-        val: 4
+        house: "Gryffindor",
+        values: [4, 5, 4, 5, 4, 2, 2, 3, 3, 5],
         image: "https://raw.githubusercontent.com/kat-zhang/house_match/master/app/public/assets/images/gryffindor.jpg"
     },
     {
         house: "Hufflepuff",
-        val: 5,
+        values: [1, 2, 2, 5, 5, 1, 4, 2, 2, 2,],
         image: "https://raw.githubusercontent.com/kat-zhang/house_match/master/app/public/assets/images/hufflepuff.jpg"
     },
-]
+];
