0081e7c865515634da616daaefd7fa293a012984
kat zhang <kzhang193@gmail.com>
2018-12-20 19:27:02 -0600

    new questions for new logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,7 @@
-var mysql = require("mysql");
 
 
-var connection;
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3307,
-        user: "root",
-        password: "root",
-        database: "survey_db"
-    });
-}
-
-connection.connect(function (error) {
-    if (error) {
-        console.error("error connecting: " + error.stack);
-    }
-});
+// connection.connect(function (error) {
+//     if (error) {
+//         console.error("error connecting: " + error.stack);
+//     }
+// });
