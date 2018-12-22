19b131e937b47ba69d5ab5843524d92abe4c62cb
kat zhang <kzhang193@gmail.com>
2018-12-21 20:27:29 -0600

    gitignore

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,34 @@
-var house = require()
+var mysql = require("mysql");
 
-// connection.connect(function (error) {
-//     if (error) {
-//         console.error("error connecting: " + error.stack);
-//     }
-// });
+var connection;
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "root",
+  database: "sort_db"
+});
+}
+
+connection.connect(function(err) {
+if (err) {
+  console.error("error connecting: " + err.stack);
+  
+}
+});
+
+function loadResults() {
+    // Selects all of the data from the MySQL profiles table
+    connection.query("SELECT * FROM houses", function(err, res) {
+      if (err) throw err;
+      //a fun trick for converting mysql's returned 'rowPacketData' obj into more usable JSON
+      var data = JSON.stringify(res);
+      data = JSON.parse(data);
+      // loop over your data converting the string of numbers into an array (using split??)
+      friends = data;
+    });
+  }
+  
