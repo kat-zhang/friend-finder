635996fc1cf4526399d6aa9c4ba09bf3d388573f
kat zhang <kzhang193@gmail.com>
2018-12-22 23:27:08 -0600

    on click function

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,4 +8,10 @@ module.exports = function(app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+    
+    //Universal Route
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+   
 }
